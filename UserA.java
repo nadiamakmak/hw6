1cff62678516304c4806d4b8168d60a76a57f397
Nadia Makmak <makmak@usc.edu>
2023-04-18 18:55:16 -0700

    ignore

---
+++ UserA.java
@@ -0,0 +1,12 @@
+public class UserA {
+    private String dbUrl;
+    //url of database
+    private String dbUser;
+    //username to connect to database
+    private String dbPass;
+    //password
+    public boolean authenticate(String username, String password){
+        
+        return true;
+    }
+}
